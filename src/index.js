e81836cb0fc786f31222df7aa92111108993acb4
Victoria E Chen <victoria.c6140@mgt3.cluster>
2024-09-12 14:27:33 -0500

    move composer environment to repo

---
+++ src/index.js
@@ -0,0 +1,347 @@
+import React, { useState, useEffect, useRef, createContext } from "react";
+
+import ReactDOM from "react-dom";
+import Text from "./dashboard_elements/Text";
+import Select from "./dashboard_elements/Select";
+import Picker from "./dashboard_elements/Picker";
+import Composer from "./dashboard_elements/Composer";
+import MultiPaneTextArea from "./dashboard_elements/MultiPaneTextArea"
+export const GlobalFilesContext = createContext();
+
+function App() {
+  const [globalFiles, setGlobalFiles] = useState([]);
+  const [environment, setEnvironment] = useState({env: "", src: ""});
+  const [fields, setFields] = useState({});
+  const [jobScript, setJobScript] = useState("");
+  const [warningMessages, setWarningMessages] = useState([])
+
+
+  const [panes, setPanes] = useState([{title: "", name: "",content: ""}])
+
+  const formRef = useRef(null);
+  const previewRef = useRef(null);
+  const multiPaneRef = useRef(null);
+
+  const [defaultLocation, setDefaultLocation] = useState(
+    "/scratch/user/" + document.user + "/job_composer"
+  );
+
+  // document.dashboard_url = "/pun/dev/React-Rewrite";
+
+  const [environments, setEnvironments] = useState([]);
+  useEffect(() => {
+    fetch(document.dashboard_url + "/jobs/composer/environments")
+      .then((response) => response.json())
+      .then((data) => {
+        setEnvironments(data.map((env) => ({ value: env.env, label: env.env, src: env.src })))
+      })
+      .catch((error) => {
+        console.error("Can't fetch dashboard URL: ", document.dashboard_url);
+        // console.error("Error fetching JSON data");
+      });
+  }, []);
+
+  function sync_job_name(name) {
+    setDefaultLocation(
+      "/scratch/user/" + document.user + "/job_composer/" + name
+    );
+  }
+
+ function handleEnvChange(key, option) {
+    const env = option.getAttribute("value") 
+    const src = option.getAttribute("src");
+
+    setEnvironment({env: env, src: src});
+	 
+    fetch(document.dashboard_url + "/jobs/composer/schema/" + env + "?src=" + src)
+      .then((response) => response.json())
+      .then((data) => setFields(data))
+      .catch((error) => {
+        console.error("Can't fetch dashboard URL: ", document.dashboard_url);
+        // console.error("Error fetching JSON data");
+      });
+  }
+
+  function handleUploadedFiles(files, globalFiles) {
+    let combinedFiles = Array.from(new Set([...globalFiles, ...files]));
+    setGlobalFiles(combinedFiles);
+  }
+
+  function preview_job(action, formData, callback) {
+    var request = new XMLHttpRequest();
+    
+    request.responseType = "json";
+    formData.append("env_dir", environment.src)
+	  
+    request.open("POST", action, true);
+	  
+    request.onload = function (event) {
+      if (request.status == 200) {
+        var jobScript = request.response;
+        callback(null, jobScript); // Pass the result to the callback
+      } else {
+        callback(`Error ${request.status}. Try again!`); // Pass the error to the callback
+      }
+    };
+    request.onerror = function (event) {
+      callback("An error has occurred. Please try again!"); // Pass the error to the callback
+    };
+
+    request.send(formData);
+  }
+
+  function handlePreview() {
+    
+    const formData = new FormData(formRef.current);
+    
+    if(!formData.has("runtime")){
+    	alert("Environment is required.")
+	return 
+    }
+
+    const modal = new bootstrap.Modal(previewRef.current);
+    modal.toggle();
+    const action = document.dashboard_url + "/jobs/composer/preview";
+    preview_job(action, formData, function (error, jobScript) {
+      if (error) {
+        alert(error);
+      } else {
+        setJobScript(jobScript["script"]);
+	
+	const panes = [{title: "template.txt", content: jobScript["script"], name: "run_command"},
+	         {title: "driver.sh", content: jobScript["driver"], name: "driver"}]
+	
+	for(const [fname, content] of Object.entries(jobScript["additional_files"])){
+	    panes.push({title: fname, content: content, name: fname})
+	}
+        setPanes(panes) 
+
+	setWarningMessages(jobScript["warnings"])
+      }
+    });
+  }
+
+  function add_submission_loading_indicator() {
+    var submission_section = document.getElementById(
+      "job-submit-button-section"
+    );
+    if (submission_section == null) {
+      return;
+    }
+
+    var spinner = document.createElement("span");
+    spinner.id = "submission-loading-spinner";
+    spinner.className = "spinner-border text-primary";
+
+    submission_section.appendChild(spinner);
+  }
+
+  function remove_submission_loading_indicator() {
+    var spinner = document.getElementById("submission-loading-spinner");
+    if (spinner == null) {
+      return;
+    }
+
+    spinner.remove();
+  }
+
+  function submit_job(action, formData) {
+    var request = new XMLHttpRequest();
+
+    formData.append("env_dir", environment.src)
+
+    add_submission_loading_indicator();
+    request.open("POST", action, true);
+    request.onload = function (event) {
+      remove_submission_loading_indicator();
+      if (request.status == 200) {
+        alert(request.responseText);
+        window.location.reload();
+      } else {
+        alert(`Error ${request.status}. Try again!`);
+        window.location.reload();
+      }
+    };
+    request.onerror = function (event) {
+      remove_submission_loading_indicator();
+      alert("An error has occured. Please try again!");
+      window.location.reload();
+    };
+
+    request.send(formData);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+	 
+    const formData = new FormData(formRef.current);
+    
+    if(formData.get("name") === ''){
+    	alert("Job name is required.");
+	return
+    } 
+    const paneRefs = multiPaneRef.current.getPaneRefs();
+    const additional_files = {};
+    paneRefs.forEach(ref => {
+      if (ref.current) {
+	const current = ref.current;
+
+	const name = current.getAttribute("name");
+	if(name === "driver" || name === "run_command") {
+	    formData.append(current.getAttribute("name"), current.value)
+        }
+	else {
+	    additional_files[name] = current.value;
+	}
+
+      }
+    });
+    formData.append("additional_files", JSON.stringify(additional_files));
+	  
+    globalFiles.forEach((file) => {
+      formData.append("files[]", file);
+    });
+    const action = formRef.current.getAttribute("action");
+    submit_job(action, formData);
+  }
+
+  const handleJobScriptChange = (event) => {
+    setJobScript(event.target.value);
+  };
+
+  return (
+    <div>
+      <div className="card shadow">
+        <div className="card-header">
+          <h6 className="maroon-header">Job Composer</h6>
+        </div>
+        <div className="card-body">
+          <form
+            ref={formRef}
+            className="form"
+            role="form"
+            id="slurm-config-form"
+            autoComplete="off"
+            method="POST"
+            encType="multipart/form-data"
+            onSubmit={handleSubmit}
+	    onKeyDown={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+            action={document.dashboard_url + "/jobs/composer/submit"}
+          >
+            <div className="row">
+              <div className="col-lg-12">
+                <div id="job-content">
+                  <GlobalFilesContext.Provider
+                    value={{ globalFiles, setGlobalFiles }}
+                  >
+                    <Text
+                      name="name"
+                      id="job-name"
+                      label="Job Name"
+                      onNameChange={(name) => sync_job_name(name)}
+                    />
+                    <Picker
+                      name="location"
+                      label="Location"
+                      localLabel="Change"
+                      defaultLocation={defaultLocation}
+                    />
+                    <Select
+                      key="env_select"
+                      name="runtime"
+                      label="Environments"
+                      options={environments}
+                      onChange={handleEnvChange}
+                    />
+                    <Composer
+                      environment={environment}
+                      fields={fields}
+                      onFileChange={(files, globalFiles) =>
+                        handleUploadedFiles(files, globalFiles)
+                      }
+                    />
+                  </GlobalFilesContext.Provider>
+                </div>
+              </div>
+	  </div>
+            <div className="form-group row text-center">
+              <div id="job-preview-button-section" className="col-lg-12">
+                <input
+                  type="button"
+                  id="job-preview-button"
+                  className="btn btn-primary maroon-button"
+                  value="Preview"
+                  onClick={handlePreview}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="card-footer">
+          <small className="text-muted">
+            ⚠️ Cautions: Job files will overwrite existing files with the same
+            name. The same principle applies for your executable scripts.
+          </small>
+        </div>
+      </div>
+
+      {/* Modal for job preview */}
+      <div
+        ref={previewRef}
+        className="modal fade bd-example-modal-lg"
+        id="job-preview-modal"
+        tabIndex="-1"
+        role="dialog"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Job Preview</h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+	     <div
+               id="warning-messages"
+               className="alert alert-warning mt-3"
+               style={{ display: (warningMessages.length != 0) ? 'block' : 'none' }}
+             >
+	       <h6 className="alert-heading">The script was generated with the following warnings:</h6>
+	        <ul>
+                  {warningMessages.map((warning, index) => (
+                    <li key={index}>{warning}</li>
+                  ))}
+                </ul>
+             </div>
+              <div id="job-preview-container">
+	          <MultiPaneTextArea ref={multiPaneRef}  panes={panes} setPanes={setPanes} />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <div className="form-group row text-center">
+                <div id="job-submit-button-section" className="col-lg-12">
+                  <input
+                    type="submit"
+                    className="btn btn-primary maroon-button"
+                    value="Submit"
+                    form="slurm-config-form"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// Render the parent component into the root DOM node
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ views/job_composer.py
@@ -0,0 +1,223 @@
+from flask import Blueprint, render_template, request, jsonify, current_app as app
+import json
+import sqlite3
+import re
+import os
+from machine_driver_scripts.engine import Engine
+import subprocess
+
+job_composer = Blueprint("job_composer", __name__)
+
+@job_composer.route("/")
+def composer():
+    environments = _get_environments() 
+    return render_template("index_no_banner.html", environments=environments)
+
+def get_directories(path):
+    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
+@job_composer.route('/modules', methods=['GET'])
+def get_modules():
+    query = request.args.get('query')
+    toolchain = request.args.get('toolchain')
+    modules_db_path = app.config['modules_db_path'] + f'{toolchain}.sqlite3'
+
+    with sqlite3.connect(modules_db_path) as modules_db:
+        cursor = modules_db.cursor()
+        cursor.execute("SELECT name FROM modules WHERE name LIKE ?", (f'{query}%',))
+        results = cursor.fetchall()
+        module_names = [result[0] for result in results]
+
+    response_data = {'data': module_names}
+    return jsonify(response_data)
+
+@job_composer.route('/environment/<environment>', methods=['GET'])
+def get_environment(environment):
+    env_dir = request.args.get("src")
+    if env_dir is None:
+        template_path = os.path.join('environments', environment, 'template.txt')
+    else:
+        template_path = os.path.join(env_dir, environment, 'template.txt')
+
+    if os.path.exists(template_path):
+        template_data = open(template_path, 'r').read()
+    else:
+        raise FileNotFoundError(f"{os.path.join(env_dir, environment, 'template.txt')} not found")
+    
+    return template_data
+
+@job_composer.route('/schema/<environment>', methods=['GET'])
+def get_schema(environment):
+    env_dir = request.args.get("src")
+    if env_dir is None:
+        schema_path = os.path.join('environments', environment, 'schema.json')
+    else:
+        schema_path = os.path.join(env_dir, environment, 'schema.json')
+
+    if os.path.exists(schema_path):
+        schema_data = open(schema_path, 'r').read()
+    else:
+        raise FileNotFoundError(f"{os.path.join(env_dir, environment, 'schema.json')} not found")
+   
+    schema_dict = json.loads(schema_data)
+    
+    for key in schema_dict:
+        if schema_dict[key]["type"] == "dynamic_select":
+            
+            retriever_path = os.path.join(env_dir, environment, schema_dict[key]["retriever"])
+            bash_command = f"bash {retriever_path}"
+
+            try:
+                result = subprocess.run(bash_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+                if result.returncode == 0:
+                    options = json.loads(result.stdout)
+                else:
+                    return result.stderr
+            except subprocess.CalledProcessError as e:
+                return e.stderr
+            
+            schema_dict[key]["type"] = "select" 
+            schema_dict[key]["options"] = options
+
+    return json.dumps(schema_dict)
+
+
+@job_composer.route('/map/<environment>', methods=['GET'])
+def get_map(environment):
+    env_dir = request.args.get("src")
+    if env_dir is None:
+        map_path = os.path.join('environments', environment, 'map.json')
+    else:
+        map_path = os.path.join(env_dir, environment, 'map.json')
+
+    if os.path.exists(map_path):
+        map_data = open(map_path, 'r').read()
+    else:
+        raise FileNotFoundError(f"{os.path.join(env_dir, environment, 'map.json')} not found")
+    
+    return map_data
+
+def create_folder_if_not_exist(dir_path):
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+
+def save_file(file, location):
+    if not os.path.exists(location):
+        os.makedirs(location)
+
+    subpath, filename = os.path.split(file.filename)
+
+    subpath_directory = os.path.join(location, subpath)
+    file_path = os.path.join(subpath_directory, filename)
+
+    # Ensure the subpath directory exists
+    if not os.path.exists(subpath_directory):
+        os.makedirs(subpath_directory)
+
+    # Save the file
+    with open(file_path, 'wb') as f:
+        f.write(file.read())
+
+    return file_path
+    
+
+@job_composer.route('/submit', methods=['POST'])
+def submit_job():
+    params = request.form
+    files = request.files
+
+    create_folder_if_not_exist(params.get('location'))
+
+    engine = Engine()
+    engine.set_environment(params.get('runtime'), params.get('env_dir'))
+
+    # Saving Files
+    # executable_script = save_file(files.get('executable_script'), params.get('location'))
+    extra_files = files.getlist('files[]')
+    for file in extra_files:
+        save_file(file, params.get('location'))
+
+    bash_script_path = engine.generate_script(params)
+    driver_script_path = engine.generate_driver_script(params)
+    bash_command = f"bash {driver_script_path}"
+    
+    try:
+        result = subprocess.run(bash_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        if result.returncode == 0:
+                return result.stdout
+        else:
+            return result.stderr
+    except subprocess.CalledProcessError as e:
+        return e.stderr
+    
+@job_composer.route('/test_submit', methods=['POST'])
+def test_submit():
+    params = request.form
+    files = request.files
+    print(params)
+    print(files)
+    return "Success"
+
+
+@job_composer.route('/preview', methods=['POST'])
+def preview_job():
+    params = request.form
+    engine = Engine()
+    
+    engine.set_environment(params.get('runtime'), params.get('env_dir'))
+    preview_job = engine.preview_script(params)
+
+    return jsonify(preview_job)
+@job_composer.route('/mainpaths', methods=['GET'])
+def get_main_paths():
+    current_user = os.getenv("USER")
+    group_names = os.popen(f'groups {current_user}').read().split(":")[1].split()
+    group_names = [s.strip() for s in group_names]
+    
+    paths = {"Home": f"/home/{current_user}", "Scratch": f"/scratch/user/{current_user}"}
+
+    for group_name in group_names:
+        groupdir = f"/scratch/group/{group_name}"
+        if os.path.exists(groupdir):
+            paths[group_name] = groupdir
+
+    return jsonify(paths)
+
+def fetch_subdirectories(path):
+    subdirectories = [os.path.basename(entry) for entry in os.listdir(path) if os.path.isdir(os.path.join(path, entry))]
+    subfiles = [os.path.basename(entry) for entry in os.listdir(path) if os.path.isfile(os.path.join(path, entry))]
+    return {"subdirectories": subdirectories, "subfiles": subfiles}
+
+@job_composer.route('/subdirectories', methods=['GET'])
+def get_subdirectories():
+    fullpath = request.args.get('path')
+    print(fullpath)
+    subdirectories = fetch_subdirectories(fullpath)
+    return subdirectories
+
+@job_composer.route('/environments', methods=['GET'])
+def get_environments():
+    environments = _get_environments()
+    return jsonify(environments)
+
+
+def _get_environments():
+    system_environments = get_directories("./environments")
+    system_environments = [{"env": env, "src": "./environments"} for env in system_environments]
+    
+    user_envs_path = request.args.get("user_envs_path")
+
+    if user_envs_path is None:
+        user_envs_path = f"/scratch/user/{os.getenv('USER')}/drona_composer/environments" 
+
+    user_environments = []
+    try:
+        user_environments = get_directories(user_envs_path)
+        user_environments = [{"env": env, "src": user_envs_path} for env in user_environments]
+    except OSError as e:
+        print(e)
+        
+    environments = system_environments + user_environments
+
+    return environments
+
