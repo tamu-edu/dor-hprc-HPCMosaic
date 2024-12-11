53fad5517250f8f164984b00f2464ece3b59d51d
Gabriel A Floreslovo <gabr13lenstation@mgt3.cluster>
2024-12-11 15:26:00 -0600

    Wrote api to collect information from metadata file

--- src/Charts/PyVenvManager.js
+++ src/Charts/PyVenvManager.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PyVenvManager = () => {
   return (
     <div>
-      Python Virtual Environment Manager
+      
     </div>
   )
 }
--- views/api.py
+++ views/api.py
@@ -21,3 +21,20 @@ def get_sinfo():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@api.route('/get_env', methods=['GET'])
+def get_envs():
+	medataPath = os.path.expandvars("$SCRATCH/virtual_envs/metadata.json")
+	try:
+		with open(metadataPath,'r') as file:
+			metadata = json.load(file)	
+			return jsonify(metadata)
+    except FileNotFoundError as e:
+        error_msg = e.output.decode("utf-8")
+        return jsonify({"error": "There was no metadata file found; you likely have not yet used 'create_venv' to make a virtual environment: {error_msg}"})
+    except json.JSONDecodeError as e:
+        error_msg = e.output.decode("utf-8")
+        return jsonify({"error": "The metadata file is corrupted or not in JSON format: {error_msg}"})
+    except Exception as e:
+        error_msg = e.output.decode("utf-8")
+		return jsonify({"error": "There was an unexpected error: {error_msg}"})
+		
