2035386c4016e4e64804ce59f220db45be96c7db
Sahil Vartak <u.sv309862@portal.cluster>
2025-11-14 15:15:46 -0600

    Revert to commit 9a2e29ab02ae600a1231285265fefcc7a1948927

--- src/utils/generate_filepath.js
+++ src/utils/generate_filepath.js
@@ -17,24 +17,3 @@ export function generate_file_explorer_path_for_disk(disk_path) {
     </a>
   );
 }
-
-export function generate_file_explorer_path_for_jobs(job) {
-  if (!job.submit_dir) return job.job_id; // fallback if no directory
-
-  const fullUrl = `/pun/sys/dashboard/files/fs${job.submit_dir}`;
-
-  return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        color: '#003C71',
-        fontWeight: 'bold',
-        textDecoration: 'underline',
-      }}
-      href={fullUrl}
-    >
-      {job.job_id}
-    </a>
-  );
-}
